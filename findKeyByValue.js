ca9005d2dc5f21548501f1f910de45035e87e24c
Mina <minatoosa@yahoo.com>
2020-10-29 05:27:14 +0000

    Find value

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`assertion passed👍🏽🙂 :  ${actual} === ${expected}`);
+  } else
+    console.log(`assertion failed😏😏 :  ${actual}  !==  ${expected}`);
+};
+const findKeyByValue = function(object1, value){
+for (const element in object1){
+  if(object1[element] === value){
+    return value;
+  }
+}
+return undefined;
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
