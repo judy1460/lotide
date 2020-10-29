0cb2aac05c6bb7e2acc6f155b93419d0e126c782
Mina <minatoosa@yahoo.com>
2020-10-29 03:58:13 +0000

    count letters

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`assertion passed👍🏽🙂 :  ${actual} === ${expected}`);
+  } else
+    console.log(`assertion failed😏😏 :  ${actual}  !==  ${expected}`);
+};
+const countLetters = function(string) {
+  const results = {}
+  for (const letter of string) {
+   if ( string !== " ") { 
+      if (results[letter]) {
+        results[letter] ++;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+
+return results
+};
+
+const str = countLetters("my name is mina");
+assertEqual(str["m"], 3);
+assertEqual(str["y"], 1);
+assertEqual(str["r"], 0);
+assertEqual(str["a"], 3);
