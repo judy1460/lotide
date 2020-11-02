352d8c7eff8e36e0ab5e468cd3a973611b040e23
Mina <minatoosa@yahoo.com>
2020-11-02 03:19:43 +0000

    Add test tai

--- head.js
+++ head.js
@@ -1,7 +1,6 @@
 //const assertEqual = require('./assertEqual');
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 const head = function(arr){
   return arr [0];
 };
+
 module.exports = head;
--- tail.js
+++ tail.js
@@ -1,16 +1,7 @@
-const assertEqual = function(actual, expected) {
-  console.log(actual);
-  console.log(expected);
-  if (Array.isArray(actual) && Array.isArray(expected) && actual.length === expected.length && actual.every((val, index) => val === expected[index])){
-    console.log("assertion passed👍🏽🙂");
-  } else
-    console.log("assertion failed😏😏");
-
-};
-
 const tail = function(array) {
   let newArray = array.slice(1);
   return newArray;
 };
-assertEqual(tail([5,6,7]),[6,7]);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = tail;
+
