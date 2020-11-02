9760cbb62fc1730d239b5456af59267ab61c0b04
Mina <minatoosa@yahoo.com>
2020-11-02 04:28:22 +0000

    Add eqarray test

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`assertion passed👍🏽🙂 :  ${actual} === ${expected}`);
-  } else
-    console.log(`assertion failed😏😏 :  ${actual}  !==  ${expected}`);
-};
-
 const eqArrays = function ( arr1, arr2 ){
   let equal = true;
    if (arr1.length !== arr2.length ){
@@ -22,8 +15,5 @@ const eqArrays = function ( arr1, arr2 ){
    return equal;
 
      };
-
-     assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-     assertEqual(eqArrays([2, 3, 4], [1, 2, "3"]), false);
-     assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
- 
+     module.exports = eqArrays;
+     
